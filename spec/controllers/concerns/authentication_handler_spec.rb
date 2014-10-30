50ca0cadcb1d648c9b203a84cb122ea68594380d
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2014-10-30 19:34:36 -0200

    Require login in "base domain" whenever accessing channel link
    Drops the need of a IS_STAGING variable to handle what is defined by
    channels and environment definitions.

--- spec/controllers/concerns/authentication_handler_spec.rb
+++ spec/controllers/concerns/authentication_handler_spec.rb
@@ -1,44 +1,12 @@
 require 'spec_helper'
 
-describe Concerns::AuthenticationHandler do
+describe Concerns::AuthenticationHandler, type: :controller do
   let(:controller) { ApplicationController.new }
 
-  describe '#base_domain_with_https_url_params' do
-    subject { controller.base_domain_with_https_url_params }
-    context 'when rails env is production and IS_STAGING is not setted' do
-      before do
-        Rails.env.stub(:production?).and_return(true)
-        ENV['IS_STAGING'] = nil
-      end
-
-      it 'returns a hash with protocol and host' do
-        expect(subject).to eq({
-          protocol: 'https',
-          host: 'localhost'
-        })
-      end
-    end
-
-    context 'when the env is production and IS_STAGING is setted' do
-      before do
-        Rails.env.stub(:production?).and_return(true)
-        ENV['IS_STAGING'] = 'true'
-      end
-
-      it 'returns a empty hash' do
-        expect(subject).to eq({})
-      end
-    end
-
-    context 'when the env is not production and IS_STAGING is setted' do
-      before do
-        Rails.env.stub(:production?).and_return(false)
-        ENV['IS_STAGING'] = 'true'
-      end
-
-      it 'returns a empty hash' do
-        expect(subject).to eq({})
-      end
+  describe '#base_domain' do
+    it 'returns a hash with proper domain' do
+      ENV['BASE_DOMAIN'] = 'login.example.com'
+      expect(controller.base_domain).to eq(host: 'login.example.com')
     end
   end
 end
